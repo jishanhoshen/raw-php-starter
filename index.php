0e077c2b27e87364965b3eced3183687577a3f5e
Jishan Hoshen <70538593+jishanhoshen@users.noreply.github.com>
2019-02-13 04:22:04 +0600

    raw php starter

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+	<meta name="author" content="John Doe">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Demo Title</title>
+	<link rel="icon" href="favicon.png" sizes="16x16" type="image/png">
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/all.css" />
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	
+	
+	
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
